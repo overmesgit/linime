9960482458ab247c98309b8477e65880954570ca
overmes <overmes@gmail.com>
2016-10-22 17:30:51 +0300

    add background

--- shippable/success.sh
+++ shippable/success.sh
@@ -1,4 +1,4 @@
-ssh -t -o StrictHostKeyChecking=no -i /tmp/ssh/lite root@linime.animesos.net 'mkdir -p /root/build'
-ssh -t -o StrictHostKeyChecking=no -i /tmp/ssh/lite root@linime.animesos.net 'rm -r /root/build/linime'
-ssh -t -o StrictHostKeyChecking=no -i /tmp/ssh/lite root@linime.animesos.net 'mkdir -p /root/build/linime'
+ssh -o StrictHostKeyChecking=no -i /tmp/ssh/lite root@linime.animesos.net 'mkdir -p /root/build'
+ssh -o StrictHostKeyChecking=no -i /tmp/ssh/lite root@linime.animesos.net 'rm -r /root/build/linime'
+ssh -o StrictHostKeyChecking=no -i /tmp/ssh/lite root@linime.animesos.net 'mkdir -p /root/build/linime'
 scp -r -o StrictHostKeyChecking=no -i /tmp/ssh/lite * root@linime.animesos.net:/root/build/linime
