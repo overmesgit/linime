30d5137f65346c967168db9402cdc7aaf3dab638
overmes <overmes@gmail.com>
2017-11-26 13:09:17 +0300

    fix tutorial

--- shippable/success.sh
+++
@@ -1,4 +0,0 @@
-ssh -o StrictHostKeyChecking=no -i /tmp/ssh/lite root@linime.animesos.net 'mkdir -p /root/build'
-ssh -o StrictHostKeyChecking=no -i /tmp/ssh/lite root@linime.animesos.net 'rm -r /root/build/linime'
-ssh -o StrictHostKeyChecking=no -i /tmp/ssh/lite root@linime.animesos.net 'mkdir -p /root/build/linime'
-scp -r -o StrictHostKeyChecking=no -i /tmp/ssh/lite * root@linime.animesos.net:/root/build/linime
